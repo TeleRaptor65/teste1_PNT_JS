33997e266b461155ebbb5a973753bd499c0c65d3
Cauä Aguiar <117834195+TeleRaptor65@users.noreply.github.com>
2024-03-11 22:41:28 -0300

    identando parte da questão 7 para ficarem dentro da função book()

--- script.js
+++ script.js
@@ -135,41 +135,41 @@ function get_book() {
                 }
             };
 
-    livros_print = ['Jurassic Park', 'Maktub', 'A Arte da Guerra'];
+        livros_print = ['Jurassic Park', 'Maktub', 'A Arte da Guerra'];
 
-    alert('Mostrar\n\n0 - Todos os livros \n1 - Jurassic Park \n2 - Maktub \n3 - A Arte da Guerra');
+        alert('Mostrar\n\n0 - Todos os livros \n1 - Jurassic Park \n2 - Maktub \n3 - A Arte da Guerra');
 
-    livro = parseInt(prompt('Insira a numeração da sua escolha:'));
+        livro = parseInt(prompt('Insira a numeração da sua escolha:'));
 
-    switch (livro) {
-        case 0:
+        switch (livro) {
+            case 0:
 
-            alert(`Livro 1: ${livros_print[0]} \nPáginas: ${biblioteca.livro1.quantidade_paginas} \nAutor: ${biblioteca.livro1.autor} \nEditora: ${biblioteca.livro1.editora}`)
-            alert(`Livro 2: ${livros_print[1]} \nPáginas: ${biblioteca.livro2.quantidade_paginas} \nAutor: ${biblioteca.livro2.autor} \nEditora: ${biblioteca.livro2.editora}`)
-            alert(`Livro 3: ${livros_print[2]} \nPáginas: ${biblioteca.livro3.quantidade_paginas} \nAutor: ${biblioteca.livro3.autor} \nEditora: ${biblioteca.livro3.editora}`)
-            break;
-
-
-        case 1:
-
-            alert(`Páginas: ${biblioteca.livro1.quantidade_paginas} \nAutor: ${biblioteca.livro1.autor} \nEditora: ${biblioteca.livro1.editora}`);
-            break;
+                alert(`Livro 1: ${livros_print[0]} \nPáginas: ${biblioteca.livro1.quantidade_paginas} \nAutor: ${biblioteca.livro1.autor} \nEditora: ${biblioteca.livro1.editora}`)
+                alert(`Livro 2: ${livros_print[1]} \nPáginas: ${biblioteca.livro2.quantidade_paginas} \nAutor: ${biblioteca.livro2.autor} \nEditora: ${biblioteca.livro2.editora}`)
+                alert(`Livro 3: ${livros_print[2]} \nPáginas: ${biblioteca.livro3.quantidade_paginas} \nAutor: ${biblioteca.livro3.autor} \nEditora: ${biblioteca.livro3.editora}`)
+                break;
 
 
-        case 2:
+            case 1:
 
-            alert(`Páginas: ${biblioteca.livro2.quantidade_paginas} \nAutor: ${biblioteca.livro2.autor} \nEditora: ${biblioteca.livro2.editora}`);
-            break;
+                alert(`Páginas: ${biblioteca.livro1.quantidade_paginas} \nAutor: ${biblioteca.livro1.autor} \nEditora: ${biblioteca.livro1.editora}`);
+                break;
 
 
-        case 3:
+            case 2:
 
-            alert(`Páginas: ${biblioteca.livro3.quantidade_paginas} \nAutor: ${biblioteca.livro3.autor} \nEditora: ${biblioteca.livro3.editora}`);
-            break;
+                alert(`Páginas: ${biblioteca.livro2.quantidade_paginas} \nAutor: ${biblioteca.livro2.autor} \nEditora: ${biblioteca.livro2.editora}`);
+                break;
 
-        default:
-            break;
-        };
+
+            case 3:
+
+                alert(`Páginas: ${biblioteca.livro3.quantidade_paginas} \nAutor: ${biblioteca.livro3.autor} \nEditora: ${biblioteca.livro3.editora}`);
+                break;
+
+            default:
+                break;
+            };
     };
 };
 
